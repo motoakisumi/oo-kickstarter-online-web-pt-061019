9f1593dc30f0f6630c68f4fbb2fcb1058284dcba
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-11 23:43:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
 
   def back_project(object)
     @backed_projects << object
-    object.add_backer(self) unless object.add_backer.includes?(self)
+    object.add_backer(self) unless object.backers.includes?(self)
   end
 
 end
